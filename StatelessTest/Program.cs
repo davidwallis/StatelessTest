1898594c3974727047b45341e8c4d64a33bac9b9
David Wallis <david@wallis2000.co.uk>
2018-02-23 17:38:11 +0000

    tweaks for testing

--- StatelessTest/Program.cs
+++ StatelessTest/Program.cs
@@ -11,39 +11,64 @@ namespace StatelessTest
     {
         private static void Main()
         {
-            var home = new Location(null) { Name = "Home" };
-            var garden = new Location(home) { Name = "Garden" };
+            //var home = new Location(null) { Name = "Home" };
+            //var garden = new Location(home) { Name = "Garden" };
 
-            // ReSharper disable UnusedVariable
-            var rearGarden = new Location(garden) { Name = "Rear Garden" };
-          
-            var frontGarden = new Location(garden) { Name = "Front Garden" };
-            var sideGarden = new Location(garden) { Name = "Side Garden" };
+            //// ReSharper disable UnusedVariable
+            //var rearGarden = new Location(garden) { Name = "Rear Garden" };
 
-            var house = new Location(home) { Name = "House" };
-            var upstairs = new Location(house) { Name = "Upstairs" };
-            var downstairs = new Location(house) { Name = "Downstairs" };
+            //var frontGarden = new Location(garden) { Name = "Front Garden" };
+            //var sideGarden = new Location(garden) { Name = "Side Garden" };
 
-            var frontBedroom = new Location(upstairs) { Name = "Front Bedroom" };
-            var backBedroom = new Location(upstairs) { Name = "Back Bedroom" };
-            var office = new Location(upstairs) { Name = "Office" };
-            var bathroom = new Location(upstairs) { Name = "Bathroom" };
-            var upstairsHall = new Location(upstairs) { Name = "Upstairs Hall" };
+            //var house = new Location(home) { Name = "House" };
+            //var upstairs = new Location(house) { Name = "Upstairs" };
+            //var downstairs = new Location(house) { Name = "Downstairs" };
 
-            var kitchen = new Location(downstairs) { Name = "kitchen" };
-            var downstairsHall = new Location(downstairs) { Name = "DownstairsHall" };
-            var frontRoom = new Location(downstairs) { Name = "Front Room" };
+            //var frontBedroom = new Location(upstairs) { Name = "Front Bedroom" };
+            //var backBedroom = new Location(upstairs) { Name = "Back Bedroom" };
+            //var office = new Location(upstairs) { Name = "Office" };
+            //var bathroom = new Location(upstairs) { Name = "Bathroom" };
+            //var upstairsHall = new Location(upstairs) { Name = "Upstairs Hall" };
+
+            //var kitchen = new Location(downstairs) { Name = "kitchen" };
+            //var downstairsHall = new Location(downstairs) { Name = "DownstairsHall" };
+            //var frontRoom = new Location(downstairs) { Name = "Front Room" };
             // ReSharper restore UnusedVariable
 
-            // store locations
-            SaveSettings<Location>(home, Path.Combine(Environment.CurrentDirectory, "locations.xml"));
+            var locations = new List<Location>();
+            locations.Add(new Location(null) { Name = "Home" });
 
-            var locNew = LoadSettings<Location>(Path.Combine(Environment.CurrentDirectory, "locations.xml"));
+
+            locations.Add(new Location(locations.Find(x => x.Name == "home")) { Name = "House" });
+            locations.Add(new Location(locations.Find(x => x.Name == "home")) { Name = "Garden" });
+
+            locations.Add(new Location(locations.Find(x => x.Name == "garden")) { Name = "Rear Garden" });
+            locations.Add(new Location(locations.Find(x => x.Name == "garden")) { Name = "Front Garden" });
+            locations.Add(new Location(locations.Find(x => x.Name == "garden")) { Name = "Side Garden" });
+
+            locations.Add(new Location(locations.Find(x => x.Name == "home")) { Name = "House" });
+            locations.Add(new Location(locations.Find(x => x.Name == "house")) { Name = "Upstairs" });
+            locations.Add(new Location(locations.Find(x => x.Name == "house")) { Name = "Downstairs" });
+
+            locations.Add(new Location(locations.Find(x => x.Name == "upstairs")) { Name = "Front Bedroom" });
+            locations.Add(new Location(locations.Find(x => x.Name == "upstairs")) { Name = "Back Bedroom" });
+            locations.Add(new Location(locations.Find(x => x.Name == "upstairs")) { Name = "Office" });
+            locations.Add(new Location(locations.Find(x => x.Name == "upstairs")) { Name = "Bathroom" });
+            locations.Add(new Location(locations.Find(x => x.Name == "upstairs")) { Name = "Upstairs Hall" });
+
+            locations.Add(new Location(locations.Find(x => x.Name == "downstairs")) { Name = "Kitchen" });
+            locations.Add(new Location(locations.Find(x => x.Name == "downstairs")) { Name = "DownstairsHall" });
+            locations.Add(new Location(locations.Find(x => x.Name == "downstairs")) { Name = "Front Room" });
+
+            // store locations
+            //SaveSettings<List<Location>>(locations, Path.Combine(Environment.CurrentDirectory, "locations.xml"));
+
+            //var locNew = LoadSettings<Location>(Path.Combine(Environment.CurrentDirectory, "locations.xml"));
 
             //Create a device
             var device = CreateTestDevice();
             // Save device to devices.xml
-            SaveSettings<Device>(device, Path.Combine(Environment.CurrentDirectory, "devices.xml"));
+            //SaveSettings<Device>(device, Path.Combine(Environment.CurrentDirectory, "devices.xml"));
 
             // try Loading XML
 
@@ -56,7 +81,8 @@ namespace StatelessTest
             //}
 
 
-            GenerateSomeEvents(backBedroom, kitchen);
+            // GenerateSomeEvents(backBedroom, kitchen);
+            GenerateSomeEvents2(locations.Find(x => x.Name == "Back Bedroom"), locations.Find(x => x.Name == "Kitchen"));
 
 
             Console.ReadKey();
@@ -107,6 +133,24 @@ namespace StatelessTest
         }
 
         private static void GenerateSomeEvents(Location backBedroom, Location kitchen)
+        {
+            const int amountOfActivity = 1;
+
+            backBedroom.TryUpdateState(Trigger.SensorActivity);
+
+            kitchen.TryUpdateState(Trigger.SensorActivity);
+            for (var i = 0; i < amountOfActivity; i++)
+            {
+                //spme random activity
+                if (i == 4 || i == 20)
+                    backBedroom.TryUpdateState(Trigger.SensorActivity);
+
+                kitchen.TryUpdateState(Trigger.SensorActivity);
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+
+        private static void GenerateSomeEvents2(Location backBedroom, Location kitchen)
         {
             const int amountOfActivity = 1;
 
