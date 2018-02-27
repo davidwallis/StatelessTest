affbe2dd1809b7c35451ad34936ed4a017c3788c
David Wallis <david@wallis2000.co.uk>
2018-02-27 16:11:49 +0000

    Update location names

--- StatelessTest/Program.cs
+++ StatelessTest/Program.cs
@@ -43,26 +43,26 @@ namespace StatelessTest
             locations.Add(new Location(null) { Name = "Home" });
 
 
-            locations.Add(new Location(locations.Find(x => x.Name == "home")) { Name = "House" });
-            locations.Add(new Location(locations.Find(x => x.Name == "home")) { Name = "Garden" });
+            locations.Add(new Location(locations.Find(x => x.Name == "Home")) { Name = "House" });
+            locations.Add(new Location(locations.Find(x => x.Name == "Home")) { Name = "Garden" });
 
-            locations.Add(new Location(locations.Find(x => x.Name == "garden")) { Name = "Rear Garden" });
-            locations.Add(new Location(locations.Find(x => x.Name == "garden")) { Name = "Front Garden" });
-            locations.Add(new Location(locations.Find(x => x.Name == "garden")) { Name = "Side Garden" });
+            locations.Add(new Location(locations.Find(x => x.Name == "Garden")) { Name = "Rear Garden" });
+            locations.Add(new Location(locations.Find(x => x.Name == "Garden")) { Name = "Front Garden" });
+            locations.Add(new Location(locations.Find(x => x.Name == "Garden")) { Name = "Side Garden" });
 
-            locations.Add(new Location(locations.Find(x => x.Name == "home")) { Name = "House" });
-            locations.Add(new Location(locations.Find(x => x.Name == "house")) { Name = "Upstairs" });
-            locations.Add(new Location(locations.Find(x => x.Name == "house")) { Name = "Downstairs" });
+            locations.Add(new Location(locations.Find(x => x.Name == "Home")) { Name = "House" });
+            locations.Add(new Location(locations.Find(x => x.Name == "House")) { Name = "Upstairs" });
+            locations.Add(new Location(locations.Find(x => x.Name == "House")) { Name = "Downstairs" });
 
-            locations.Add(new Location(locations.Find(x => x.Name == "upstairs")) { Name = "Front Bedroom" });
-            locations.Add(new Location(locations.Find(x => x.Name == "upstairs")) { Name = "Back Bedroom" });
-            locations.Add(new Location(locations.Find(x => x.Name == "upstairs")) { Name = "Office" });
-            locations.Add(new Location(locations.Find(x => x.Name == "upstairs")) { Name = "Bathroom" });
-            locations.Add(new Location(locations.Find(x => x.Name == "upstairs")) { Name = "Upstairs Hall" });
+            locations.Add(new Location(locations.Find(x => x.Name == "Upstairs")) { Name = "Front Bedroom" });
+            locations.Add(new Location(locations.Find(x => x.Name == "Upstairs")) { Name = "Back Bedroom" });
+            locations.Add(new Location(locations.Find(x => x.Name == "Upstairs")) { Name = "Office" });
+            locations.Add(new Location(locations.Find(x => x.Name == "Upstairs")) { Name = "Bathroom" });
+            locations.Add(new Location(locations.Find(x => x.Name == "Upstairs")) { Name = "Upstairs Hall" });
 
-            locations.Add(new Location(locations.Find(x => x.Name == "downstairs")) { Name = "Kitchen" });
-            locations.Add(new Location(locations.Find(x => x.Name == "downstairs")) { Name = "Downstairs Hall" });
-            locations.Add(new Location(locations.Find(x => x.Name == "downstairs")) { Name = "Front Room" });
+            locations.Add(new Location(locations.Find(x => x.Name == "Downstairs")) { Name = "Kitchen" });
+            locations.Add(new Location(locations.Find(x => x.Name == "Downstairs")) { Name = "Downstairs Hall" });
+            locations.Add(new Location(locations.Find(x => x.Name == "Downstairs")) { Name = "Front Room" });
 
             // store locations
             BinarySerialize(locations);
